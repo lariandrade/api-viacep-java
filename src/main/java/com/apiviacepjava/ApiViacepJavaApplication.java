22a3552cb1f3b4622a034ecd1a13149158ca6aaa
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-06-27 10:09:51 -0300

    projeto criado

---
+++ src/main/java/com/apiviacepjava/ApiViacepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.apiviacepjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViacepJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiViacepJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiviacepjava/ApiViacepJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiviacepjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiViacepJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
