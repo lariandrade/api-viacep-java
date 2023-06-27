06667d94320bb85607eaed4328152e5b75bc8329
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-06-27 10:34:08 -0300

    feat: consumir viacep

--- src/main/java/com/apiviacepjava/Service/ConsomeViaCep.java
+++ src/main/java/com/apiviacepjava/Service/ConsomeViaCep.java
@@ -1,4 +1,33 @@
 package com.apiviacepjava.Service;
 
+import com.apiviacepjava.DTO.EnderecoDTO;
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 public class ConsomeViaCep {
+    public EnderecoDTO buscaEndereco(String cep) {
+
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), EnderecoDTO.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("CEP nao encontrado.");
+        }
+
+
+    }
 }
+
